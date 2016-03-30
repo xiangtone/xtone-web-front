95d8ec57dc35863262efb96dd947e632a977567d
Odevin <543237101@qq.com>
2016-03-30 14:59:14 +0800

    sss

--- x-php-web/youkapos/index.php
+++ x-php-web/youkapos/index.php
@@ -4,6 +4,7 @@
  * @copyright   Copyright(c) 2011
  * @author      yuansir <yuansir@live.cn/yuansir-web.com>
  * @version     1.0
+ *
  */
 require dirname(__FILE__).'/system/app.php';
 require dirname(__FILE__).'/config/config.php';
