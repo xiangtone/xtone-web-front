b5dcc5dece502de09e3c531c8c739a64055bbd6d
hongjiabin <hongjiabin110@sina.com>
2016-05-16 14:43:53 +0800

    mxkl礼包兑换 dev.jiabin

---
+++ mxkl-web/src/main/org/mxkl/info/Code.java
@@ -0,0 +1,54 @@
+package org.mxkl.info;
+
+public class Code {
+	private int id;
+	private String content;
+	private String wechatOpenId;
+	private String wechatUnionId;
+	private long addTime;
+	private long matchOpenIdTime;
+	private String gameName;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getWechatOpenId() {
+		return wechatOpenId;
+	}
+	public void setWechatOpenId(String wechatOpenId) {
+		this.wechatOpenId = wechatOpenId;
+	}
+	public String getWechatUnionId() {
+		return wechatUnionId;
+	}
+	public void setWechatUnionId(String wechatUnionId) {
+		this.wechatUnionId = wechatUnionId;
+	}
+	public long getAddTime() {
+		return addTime;
+	}
+	public void setAddTime(long addTime) {
+		this.addTime = addTime;
+	}
+	public long getMatchOpenIdTime() {
+		return matchOpenIdTime;
+	}
+	public void setMatchOpenIdTime(long matchOpenIdTime) {
+		this.matchOpenIdTime = matchOpenIdTime;
+	}
+	public String getGameName() {
+		return gameName;
+	}
+	public void setGameName(String gameName) {
+		this.gameName = gameName;
+	}
+	
+}
